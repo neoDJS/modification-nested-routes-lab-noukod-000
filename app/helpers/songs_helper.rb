580bd407ae4b29dc2b2da88f180a46a7f42b6f5d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-29 07:19:07 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
   def artist_select(song)
     if song.artist.nil?
-      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "post[author_id]", post.author_id
     end
