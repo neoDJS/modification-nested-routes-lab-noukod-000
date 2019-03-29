71b20f3ff0fa8542b86ed18f735ad6ddb12325f9
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-29 07:03:37 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exist?(params[:artist_id])
+      redirect_to artist_songs_path(@artist), alert: "Song not found"
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
