ace8d731977339525f98952feffa2290e51148e8
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-29 07:29:04 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,8 +48,8 @@ class SongsController < ApplicationController
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found."
       else
-        @song = artist.songs.find_by(id: params[:id])
-        redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
+        @song = @artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(@artist), alert: "Song not found." if @song.nil?
       end
     else
       @song = Song.find(params[:id])
